c5ca984728ace1bac8d4aa49d892adab9b14f3f9
Silurana <silurana330@gmail.com>
2026-07-10 01:29:17 +0530

    enums in ts

---
+++ src/12-enums.ts
@@ -0,0 +1,32 @@
+enum Direction {
+    Up = 8,
+    Down,
+    Left,
+    Right
+}
+
+let dir: Direction = Direction.Down // 9
+
+// String enum
+enum Statuses  {
+    Pending = "PENDING",
+    Approved = "APPROVED",
+    Rejected = "REJECTED"
+}
+
+let sts: Statuses = Statuses.Approved
+
+// Const enum (more performant)
+const enum HttpStatus {
+    OK = 200,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404
+}
+
+function handleResponse(status: HttpStatus): void {
+    if(status === HttpStatus.OK) {
+        console.log("Success!");
+        
+    }
+}
